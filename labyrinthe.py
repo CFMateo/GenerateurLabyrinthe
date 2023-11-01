cb9797172a59180faaf1a9a2aa04f299735ad429
Mateito <mateolucascf@gmail.com>
2023-10-31 23:18:52 -0400

    V.1a
    ca fonctionne, coute que coute, moins ma logique plus dans l optique de devoir rendre le devoir a minuit bb

--- labyrinthe.py
+++ labyrinthe.py
@@ -61,23 +61,24 @@ def voisins(x, y, largeur, hauteur):
         voisinNord = (coordoneesNord[0] + coordoneesNord[1] * largeur) 
         voisins.append(voisinNord)
 
-    if 0 <= coordoneesEst[0] <= largeur-1 and 0 <= coordoneesEst[1] <= hauteur-1:
-        voisinEst= (coordoneesEst[0] + coordoneesEst[1] * largeur) 
-        voisins.append(voisinEst)
+    if 0 <= coordoneesOuest[0] <= largeur-1 and 0 <= coordoneesOuest[1] <= hauteur-1:
+        voisinOuest = (coordoneesOuest[0] + coordoneesOuest[1] * largeur)
+        voisins.append(voisinOuest)
+    
     
     if 0 <= coordoneesSud[0] <= largeur-1 and 0 <= coordoneesSud[1] <= hauteur-1:
         voisinSud = (coordoneesSud[0] + coordoneesSud[1] * largeur) 
         voisins.append(voisinSud)
     
-
-    if 0 <= coordoneesOuest[0] <= largeur-1 and 0 <= coordoneesOuest[1] <= hauteur-1:
-        voisinOuest = (coordoneesOuest[0] + coordoneesOuest[1] * largeur)
-        voisins.append(voisinOuest)
+    if 0 <= coordoneesEst[0] <= largeur-1 and 0 <= coordoneesEst[1] <= hauteur-1:
+        voisinEst= (coordoneesEst[0] + coordoneesEst[1] * largeur) 
+        voisins.append(voisinEst)
     
 
     return voisins
 
-#print(voisins(4, 3, 8, 4))
+print(voisins(7, 2, 8, 4))
+
 
 
 
@@ -97,116 +98,114 @@ def laby(largeur,hauteur,dimension): #PAS OUBLIER LE PARAMETRE DIMENSION
             setPixel(i, j, "#FFF")
     
     """
-    celluleCavite = []
+    from random import randint
+
+def laby(largeur, hauteur, dimension):
+    """
+    Crée un labyrinthe aléatoire (largeur=nx et hauteur=ny)
+    et dessine le labyrinthe dans une fenêtre de pixels.
+    """
+
+    cellules_cavite = []
     frontiere = []
-    mursCavite = []
-
-    index = 0
+    
+    murs_horizontaux = sequence(largeur * (hauteur + 1))
+    murs_verticaux = sequence((largeur + 1) * hauteur)
+    
    
-
-
-    murHorizontaux = sequence(largeur*(hauteur+1))
-    murVerticaux = sequence((largeur+1)*hauteur)
-    #print(murHorizontaux)
-    #print(murVerticaux)
+    # On choisit aléatoirement la cavité initiale, en faisant en sorte que le numéro represente bien une cellule:
     caviteInit = 0
+    while 0 <= caviteInit <= largeur - 1 or largeur * (hauteur + 1) - largeur - 1 <= caviteInit <= largeur * (hauteur + 1) - 1:
+        caviteInit = randint(0, len(murs_horizontaux) - 1)
+    ajouter(cellules_cavite, caviteInit)
 
-    # Sélection aléatoire de la cavité initiale:
-    while 0<=caviteInit<=largeur-1 or largeur*(hauteur+1)-largeur-1<=caviteInit<=largeur*(hauteur+1)-1:
-        caviteInit = randint(0, len(murHorizontaux) - 1)
-    ajouter(celluleCavite,caviteInit)
+    # Premier élement = x , deuxieme = y:
+    coordonneCavite = [cellules_cavite[0] % largeur, cellules_cavite[0] // largeur]
+
+
+    
+    # On ajoute à la frontiere les voisins de la cavite initiale:
+    frontiere = voisins(coordonneCavite[0], coordonneCavite[1], largeur, hauteur)
+    
+  
+
+    while len(cellules_cavite) != largeur * hauteur:
+        frontiere_aleatoire = frontiere[randint(0, len(frontiere) - 1)]
+        x, y = frontiere_aleatoire % largeur, frontiere_aleatoire // largeur
+
+        voisins_cavite = []
+
+        if y > 0 and (x + (y - 1) * largeur) in cellules_cavite:
+            voisins_cavite.append(x + (y - 1) * largeur)
+        if y < hauteur - 1 and (x + (y + 1) * largeur) in cellules_cavite:
+            voisins_cavite.append(x + (y + 1) * largeur)
+        if x > 0 and (x - 1 + y * largeur) in cellules_cavite:
+            voisins_cavite.append(x - 1 + y * largeur)
+        if x < largeur - 1 and (x + 1 + y * largeur) in cellules_cavite:
+            voisins_cavite.append(x + 1 + y * largeur)
+
+        if voisins_cavite:
+            voisin = voisins_cavite[randint(0, len(voisins_cavite) - 1)]
+
+            if voisin == frontiere_aleatoire + 1:
+                retirer(murs_verticaux, y * (largeur + 1) + x + 1)
+            elif voisin == frontiere_aleatoire - 1:
+                retirer(murs_verticaux, y * (largeur + 1) + x)
+            elif voisin == frontiere_aleatoire + largeur:
+                retirer(murs_horizontaux, (y + 1) * largeur + x)
+            elif voisin == frontiere_aleatoire - largeur:
+                retirer(murs_horizontaux, y * largeur + x)
+
+            ajouter(cellules_cavite, frontiere_aleatoire)
+            retirer(frontiere, frontiere_aleatoire)
+
+            # Directions relatives des voisins
+            voisins_relatifs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+            for voisin_x, voisin_y in voisins_relatifs:
+                new_x, new_y = x + voisin_x, y + voisin_y
+                new_cell = new_x + new_y * largeur
+
+                # Vérification si la nouvelle cellule doit etre ajoutée a la fronti
+                if 0 <= new_x < largeur and 0 <= new_y < hauteur and new_cell not in cellules_cavite and new_cell not in frontiere:
+                    ajouter(frontiere, new_cell)
+
+
+  
+
+    print(murs_horizontaux,murs_verticaux)
     
 
-   
+   #Représentation du labyrinthe:
+    setScreenMode((largeur*dimension)+1,(hauteur*dimension)+1)
+    
+    retirer(murs_horizontaux,0)
+    print(murs_horizontaux[-1])
+    retirer(murs_horizontaux,murs_horizontaux[-1])
+    
+    
 
-    while len(celluleCavite) != largeur*hauteur:
-        mursVoisins = []
+    setScreenMode((largeur*dimension)+1,(hauteur*dimension)+1)
 
-        coordonneCavite = [celluleCavite[index] % largeur, celluleCavite[index] // largeur]
-        # Numeros frontieres a la cavité:
-        voisinsCavite = voisins(coordonneCavite[0], coordonneCavite[1], largeur, hauteur)
-        for el in voisinsCavite:
-            ajouter(frontiere,el)
-        for i in range(len(celluleCavite)):
-            retirer(frontiere,celluleCavite[i])
-            retirer(voisinsCavite)
+    for x in range(largeur*dimension):
+        for y in range(hauteur*dimension):
+            set_pixel(x,y,"#FFF")
         
-        
-        print("cavite=",celluleCavite,"////frontiere=",frontiere)
-
-        # Choix aleatoire parmis les numéros de la frontiere:
-        voisinAleatoire = frontiere[randint(0,len(frontiere)-1)]
-        coordonneVoisin = [voisinAleatoire% largeur, voisinAleatoire// largeur]
-        print(voisinAleatoire)
-
-        # On detérmine tout les murs des membres de la cavité:
-        mursCavite.append(coordonneCavite[0] + coordonneCavite[1] * largeur)  # Mur Nord
-        mursCavite.append(1 + coordonneCavite[0] + coordonneCavite[1] * (largeur + 1))
-        mursCavite.append(coordonneCavite[0] + (coordonneCavite[1] + 1) * largeur)
-        mursCavite.append(coordonneCavite[0] + coordonneCavite[1] * (largeur + 1))
-        print("mursCavite=",mursCavite)
-        
-        # On detérmine tout les murs des membres du voisin aléatoire:
-        mursVoisins.append(coordonneVoisin[0] + coordonneVoisin[1] * largeur)
-        mursVoisins.append(1 + coordonneVoisin[0] + coordonneVoisin[1] * (largeur + 1))
-        mursVoisins.append(coordonneVoisin[0] + (coordonneVoisin[1] + 1) * largeur)
-        mursVoisins.append(coordonneVoisin[0] + coordonneVoisin[1] * (largeur + 1))
-        print("MURS VOSIINS=",mursVoisins)
-
-        
-
-        # On cherche un mur commun entre la cellule aléatoire voisine et la cavité:
-        for i in range(len(mursVoisins)):
-                    if mursVoisins[i] in mursCavite:
-                        ajouter(celluleCavite,voisinAleatoire)
-                        retirer(frontiere, voisinAleatoire)
-                        retirer(mursCavite,mursVoisins[i])
-                        if i % 2 == 0:
-                            retirer(murHorizontaux, mursVoisins[i])
-                            retirer(mursVoisins,mursVoisins[i])
-                            break
-                        else:
-                            retirer(murVerticaux, mursVoisins[i])
-                            retirer(mursVoisins,mursVoisins[i])
-                            break
-                            
-                        
-        #On ajoute les murs de la cellule voisine chosie a la cavité:
-        mursCavite.extend(mursVoisins)
-        #print(murHorizontaux,murVerticaux)
-        index+=1
-        print("CELLULE CAVITE =",celluleCavite,"FRONTIERE =",frontiere,"MURS CAVITE=",mursCavite,"MURS VOISINS=",mursVoisins)
-        print(murHorizontaux,murVerticaux)
-    """
-    #Représentation du labyrinthe:
-    setScreenMode(largeur*dimension,hauteur*dimension)
 
     for y in range(hauteur):
         for x in range(largeur):
-            # Murs horizontaux:
-            if contient(murHorizontaux,x+y*largeur):
+            # Murs murHorizontauxontaux:
+            if contient(murs_horizontaux,x+y*largeur):
                 for i in range(dimension):
-                    set_pixel(x*dimension+i,y*dimension,"#FFF")
+                    set_pixel(x*dimension+i,y*dimension,"#000")
 
-            # Murs verticaux:
-            if contient(murVerticaux,x+y*(largeur+1)):
+            # Murs murVerticauxcaux:
+            if contient(murs_verticaux,x+y*(largeur+1)):
                 for i in range(dimension):
-                    set_pixel(x*dimension,y*dimension+i,"#FFF   ")
+                    set_pixel(x*dimension,y*dimension+i,"#000")
 
+                    
+                    
     
-        """
-
-
 
 laby(8,4,10)
-
-
-
-
-        
-
-
-
-
-
-
